ef5a9dcf7385bb26103a85176907680b90e34938
liljohnpork <joeybuckleton@gmail.com>
2026-08-22 16:51:21 +0100

    version alpha_0.14
    added easter eggs
    USERNAME AND PASSWORD SYSTEM!
    hashing of password for strength coming soon

--- terminal_alpha.py
+++ terminal_alpha.py
@@ -1,5 +1,7 @@
 #import the stuff
-#quote of the update: "sorry ladies, im busy coding"
+#quote of the update: "wait what, this is C#?"
+
+# todo: add more microapps
 
 import termios
 import select
@@ -9,25 +11,58 @@ import calculator_microapp
 import randomnumber_microapp
 import tty
 
-#i will leanr what this means later...
+#i will learn what this means later...
 fd = sys.stdin.fileno()
 old_settings = termios.tcgetattr
 
-print("boot")
+try:
+    with open("usr.dat", "r") as usrn:
+        usr = usrn.read()
+except FileNotFoundError:
+    with open("usr.dat","w+r") as usrn:
+        usrn.write(input("Enter username: "))
+        usr = usrn.read()
+    
+try:
+    with open("pas.dat", "r") as pasw:
+        passw = pasw.read()
+except FileNotFoundError:
+    with open("pas.dat", "w+") as pasw:
+        pasw.write(input("Enter password: "))
+        passw = pasw.read()
+
+print(f"Entering as {usr}")
 while True:
-    usr = input("Enter your username: ")
-    if len(usr) <= 10:
-        break
-    else: print("Username must be 10 or less characters.")
+    temp = input(f"Enter password for {usr}: ")
+    if temp == passw:
+         break
+    else:  
+        print("Incorrect password")
+
+helplist = [
+    "ver",
+    "raminf",
+    "esc",
+    "cpuinf",
+    "help" ,
+    "whoami" ,
+    "liveram" ,
+    "ls microapp" ,
+    "run microapp"
+    ""
+]
+
+print("booted")
+#get it working
 #loop for it
 while True:
     print('')
     cmd =  input(f"{usr}@pyggy:~$ ")
     if cmd == "ver":
-        print("alpha_0.13")
+        print("alpha_0.14")
     elif cmd == "raminf":
         mem = psutil.virtual_memory()
-        print(mem.percent, "in use of", mem.total)
+        print(str(mem.percent)+"%" " in use of", mem.total)
       
     elif cmd == "esc":
         sys.exit()
@@ -37,15 +72,8 @@ while True:
     elif cmd == "cpuinf":
         print(psutil.cpu_count(), "cores")
     elif cmd == "help":
-        print("ver")
-        print("raminf")
-        print("esc")
-        print("cpuinf")
-        print("help")
-        print("liveram")
-        print("whoami")
-        print("ls microapp")
-        print("run microapp")
+        for word in helplist:
+            print(word)
     elif cmd == "whoami":
         print(usr)
     elif cmd == "run microapp":
@@ -65,13 +93,13 @@ while True:
     elif cmd == "liveram":
         while True:
             tty.setcbreak(fd)
-            #this crap really damn annoyed me: its finally fixed
+            #this really damn annoyed me: its finally fixed: probably
             mem = psutil.virtual_memory()
             print(f"\rram: {mem.percent}%", end="")
             if select.select([sys.stdin], [], [], 0)[0]:
-               sys.stdin.read(1)  # consume the key
+               sys.stdin.read(1)  # consume the key (om nom nom)
                break
-               termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
             
     else:
         print(f"Unkown command: {cmd}")
+# i have to add these cos everyone likes comments
